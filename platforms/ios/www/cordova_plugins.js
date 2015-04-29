c5eda63eb3bc4cd1232382a4056ee73dcf56cf51
Yogesh <yogesh@zendrive.com>
2015-04-29 19:56:06 +0530

    Updated sample to use plugin version 2.0.0

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,11 +1,25 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.plugin.ActivityIndicator/www/activityIndicator.js",
+        "id": "org.apache.cordova.plugin.ActivityIndicator.ActivityIndicator",
+        "clobbers": [
+            "ActivityIndicator"
+        ]
+    },
+    {
+        "file": "plugins/com.zendrive.phonegap.sdk/www/zendrive.js",
+        "id": "com.zendrive.phonegap.sdk.ZendriveCordovaPlugin",
+        "clobbers": [
+            "Zendrive"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "android.support.v4": "21.0.1",
-    "com.google.playservices": "21.0.0",
-    "com.zendrive.phonegap.sdk": "1.3.0"
+    "org.apache.cordova.plugin.ActivityIndicator": "1.0.0",
+    "com.zendrive.phonegap.sdk": "2.0.0"
 }
 // BOTTOM OF METADATA
 });
