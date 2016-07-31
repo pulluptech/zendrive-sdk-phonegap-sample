4da75858531a5a0ef162f101d3f3ef9a845d5556
Yogesh <yogesh@zendrive.com>
2016-07-31 18:24:27 +0530

    Cordova sample updated to 2.3.1

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -21,7 +21,7 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "org.apache.cordova.plugin.ActivityIndicator": "1.0.0",
-    "com.zendrive.phonegap.sdk": "2.3.0",
+    "com.zendrive.phonegap.sdk": "2.3.1",
     "cordova-plugin-multidex": "1.0.0"
 }
 // BOTTOM OF METADATA
